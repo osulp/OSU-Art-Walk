c4a4f706828ca84b0fe1e41386d7b3faa0f514bc
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-07-02 17:00:56 -0700

    Got stem word search working

---
+++ spec/features/searching_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "searching" do
+  let(:item) {create(:art_piece)}
+  context "when there is a an item" do
+    let(:search_string) {""}
+    before do
+      item
+      visit root_path
+      fill_in "Search...", :with => search_string
+      click_button "Search"
+    end
+    context "and a blank string is searched" do
+      it "should find it" do
+        expect(page).to have_selector(".document")
+        expect(page).to have_content(item.title)
+      end
+    end
+    context "and a part is searched for" do
+      let(:item) {create(:art_piece, :title => "Warmest")}
+      let(:search_string) {"warm"}
+      it "should find it" do
+        expect(page).to have_selector('.document')
+      end
+    end
+    context "and its title is searched" do
+      let(:search_string) {item.title}
+      it "should find it" do
+        expect(page).to have_selector(".document")
+        expect(page).to have_content(item.title)
+      end
+    end
+  end
+end
