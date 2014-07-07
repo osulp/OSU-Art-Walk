e19867d36e7f28eed02182b128db9682d397b43d
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-07-07 11:12:01 -0700

    drop artist from table. create artist migration

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20140707223635) do
     t.datetime "updated_at"
   end
 
+  create_table "artists", force: true do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.string   "website"
+    t.string   "birthdate"
+    t.string   "deathdate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
     t.string   "user_type"
