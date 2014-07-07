a5b4ee8231b8eb6258b932b177e616ebf48aeea4
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-07-07 16:01:01 -0700

    Removed files, updated spec

--- spec/features/admin_update_user_status_spec.rb
+++ spec/features/admin_update_user_status_spec.rb
@@ -25,14 +25,14 @@ describe "admin update user status" do
         expect(page).to have_content("List of Users")
       end
       it "should show an admin status for the current user" do
-        within "#userstatus-#{user.id} .status" do
+        within "#userstatus-#{user.id}" do
           expect(page).to have_content("Admin")
         end
       end
       context "and there are two users" do
         let(:user_2) {create(:user)}
         it "should show a user status for the current user" do
-          within "#userstatus-#{user_2.id} .status" do
+          within "#userstatus-#{user_2.id}" do
             expect(page).to have_content("User")
           end
         end
