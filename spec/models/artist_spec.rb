8b63bef889e85b8438b617892cbf92e8dae049ab
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-07-08 10:04:51 -0700

    finished up artist model

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -12,12 +12,24 @@ describe Artist do
     context "When an art piece is set" do
 
       before do
-        #subject.art_pieces = [artpiece]
-        #subject.save
+        subject.art_pieces = [artpiece]
+        subject.save
       end
 
       it "should work" do
-        #expect(subject.art_pieces).to eq artpiece
+        expect(subject.art_pieces).to eq [artpiece]
+      end
+
+      context "when an art piece is removed" do
+
+        before do
+          artpiece.artist = nil
+          subject.save
+        end
+
+        it "should not be there" do
+          expect(artpiece.artist).to eq nil
+        end
       end
     end
   end
