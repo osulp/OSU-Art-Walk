130561279cdda36548f4c00661206d591cefad2d
balesh2 <balesh@onid.oregonstate.edu>
2014-07-17 08:49:18 -0700

    adds spacing between controller actions

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -5,30 +5,39 @@ class Admin::SettingsController < AdminController
   def index
     @setting = Setting.all
   end
+
   def new
     @setting = Setting.new
   end
+
   def create
     @setting = Setting.new(setting_params)
     flash[:success] = "Successfully Created Setting" if @setting.save
     respond_with @setting, :location => admin_settings_path
   end
+
   def edit
   end
+
   def update
     flash[:success] = "Successfully Updated Setting" if @setting.update_attributes(setting_params)
     respond_with @setting, :location => admin_settings_path
   end
+
   def destroy
     flash[:success] = "Successfully Deleted Setting" if @setting.destroy
     redirect_to admin_settings_path
   end
 
+
   private
+
   def setting_params
     params.require(:setting).permit(:copyright, :email, :about)
   end
+
   def find_setting
     @setting = Setting.find(params[:id])
   end
+
 end
