dd18a501e9ee6a0963887ca7b7ec626433ba2595
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-07-17 11:15:18 -0700

    removed temporary_until from catalog controller

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -29,7 +29,6 @@ class CatalogController < ApplicationController
     config.add_show_field 'size_ss', :label => 'Size'
     config.add_show_field 'legal_info_ss', :label => 'Legal Info'
     config.add_show_field 'creation_date_ss', :label => 'Created On'
-    config.add_show_field 'temporary_until_ss', :label => 'Temporary Until'
     config.add_show_field 'contact_info_ss', :label => 'Contact Info'
     config.add_show_field 'artists_sms', :label => 'Artists'
     config.add_show_field 'building_ss', :label => 'Building'
