b4c469a837a158caf9b4b11b5274ec35a88805c9
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-07-18 16:31:47 -0700

    Fixed about us spec

--- spec/features/about_us_spec.rb
+++ spec/features/about_us_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'About Us' do
   let(:user) {create(:user, :admin)}
+  let(:setting) {create(:setting, :setting_name => "About")}
   before do
     user
     capybara_login(user) if user
@@ -15,12 +16,14 @@ describe 'About Us' do
       expect(page).to have_link("About Us")
     end 
 
-    context "should have link to about us" do
+    context "and the link is clicked" do
       before do
+        setting
         click_link "About Us"
       end
-      it "and route to about us page" do
-        expect(page).to have_content("About Us!")
+
+      it "should display the about us information" do
+        expect(page).to have_content(setting.value)
       end
     end
   end
