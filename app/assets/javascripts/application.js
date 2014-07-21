30e27e90f8e36eaf6b252cab08d407121076b151
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-07-21 15:04:20 -0700

    can add multiple photos to an art piece

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,18 @@
 //= require blacklight/blacklight
 //= require_tree .
 //= require tinymce-jquery
+
+
+function remove_fields(link){
+  $(link).prev('input[type=hidden]').val("1");
+  $(link).closest('.fields').hide();
+}
+
+$(document).ready(function(){
+  $('form').on('click', '.add_fields', function (event){
+    time = new Date().getTime()
+    regexp = new RegExp($(this).data('id'), 'g')
+    $(this).before($(this).data('fields').replace(regexp, time))
+    event.preventDefault()
+  });
+});
--- spec/features/multi_photo_spec.rb
+++ spec/features/multi_photo_spec.rb
@@ -12,7 +12,6 @@ describe "Multi Photo" do
 
   context "When adding a single photo" do
     before do
-      #binding.pry
       fill_in "Title", :with => "Sample Title"
       within ("#picture") do
         attach_file("art_piece_art_piece_photos_attributes_0_photo", "app/assets/images/cats.jpg")
@@ -22,6 +21,27 @@ describe "Multi Photo" do
 
     it "should save and display it" do
       expect(page).to have_css("img")
+    end
+  end
+
+  context "When adding multiple photos", :js => true do
+    before do
+      fill_in "Title", :with => "Sample Title"
+      click_link "Add Another Photo"
+      photos = all("*[id^=art_piece_art_piece_photos]")
+      photos.each do |photo_wrapper|
+        attach_file photo_wrapper[:id], Rails.root.join("app","assets","images","cats.jpg")
+      end
+      click_button "Create Art piece"
+      expect(page).to have_content("Successfully created art piece")
+      @piece = ArtPiece.first
+    end
+
+    it "should save and display them" do
+      click_link @piece.title
+      within("#piece-#{@piece.id}") do
+        expect(page).to have_selector('img', :count => 2)
+      end
     end
   end
 
