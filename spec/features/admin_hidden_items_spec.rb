bc504d0637bedcaad819be14135078ef843a48fe
balesh2 <balesh@onid.oregonstate.edu>
2014-07-21 15:58:54 -0700

    adds Displayed facet for admin- hides for nonadmin

--- spec/features/admin_hidden_items_spec.rb
+++ spec/features/admin_hidden_items_spec.rb
@@ -10,20 +10,35 @@ describe "hidden items" do
   end
 
   context "when on the index page" do
+    let(:user) {create(:user)}
+    let(:item) {create(:art_piece, :with_building, :displayed => true )}
+    before do
+      item
+      visit root_path
+      click_button "Search"
+    end
+    it "should not have the Displayed facet" do
+      expect(page).not_to have_link("Displayed")
+    end
+    it "should have other facets" do
+      expect(page).to have_link("Building")
+    end
+  end
+  context "when on the index page as an admin" do
     let(:user) {create(:user, :admin)}
     before do
       visit root_path
     end
-    it "should be logged in as an admin" do
-      expect(page).to have_link("Admin Panel")
-    end
-    context "when searching for all art pieces" do
+    context "when searching for all art pieces as admin" do
       before do
         item
         click_button "Search"
       end
       it "should display the item where 'displayed' is false" do
         expect(page).to have_selector(".document")
+      end
+      it "should display the 'Displayed' facet" do
+        expect(page).to have_link("Displayed")
       end
     end
   end
