2b67014a4ec2a9a2501773055d405429d1c96aa6
balesh2 <balesh@onid.oregonstate.edu>
2014-07-24 14:19:16 -0700

    makes reindexing function private, calls aftersave

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,6 +1,7 @@
 class Building < ActiveRecord::Base
   has_many :art_pieces, :through => :art_piece_buildings
   has_many :art_piece_buildings
+  after_save { reindex_art_pieces }
 
   def coords
     [name, lat, long].join("-|-")
@@ -8,6 +9,8 @@ class Building < ActiveRecord::Base
 
   private
 
-  after_save { art_pieces.reindex }
+  def reindex_art_pieces
+    art_pieces.reindex
+  end
 
 end
