976bc41697b473f769a3ce686ccbfad3d7b129bf
balesh2 <balesh@onid.oregonstate.edu>
2014-07-24 14:27:49 -0700

    changes reindex call to symbol notation

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,7 +1,7 @@
 class Building < ActiveRecord::Base
   has_many :art_pieces, :through => :art_piece_buildings
   has_many :art_piece_buildings
-  after_save { reindex_art_pieces }
+  after_save :reindex_art_pieces
 
   def coords
     [name, lat, long].join("-|-")
