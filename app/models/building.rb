0250abaaee7a4838baf309d8f8940cd9b9b0d11a
balesh2 <balesh@onid.oregonstate.edu>
2014-07-24 14:06:38 -0700

    makes the reindexing private to the model

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,9 +2,12 @@ class Building < ActiveRecord::Base
   has_many :art_pieces, :through => :art_piece_buildings
   has_many :art_piece_buildings
 
-  after_save { art_pieces.reindex }
-
   def coords
     [name, lat, long].join("-|-")
   end
+
+  private
+
+  after_save { art_pieces.reindex }
+
 end
