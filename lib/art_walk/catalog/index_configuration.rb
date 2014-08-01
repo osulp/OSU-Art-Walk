4d21f911b2201a5b3757fbcad97c823e4390252a
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-08-01 08:36:24 -0700

    added medium to search and index view

--- lib/art_walk/catalog/index_configuration.rb
+++ lib/art_walk/catalog/index_configuration.rb
@@ -7,6 +7,7 @@ module ArtWalk
           config.index.title_field = "title_ss"
           config.index.thumbnail_field = "art_piece_photos_sms"
           config.add_index_field 'size_ss', :label => 'Size'
+          config.add_index_field 'medium_ss', :label => 'Medium'
           config.add_index_field 'creation_date_ss', :label => 'Created On'
           config.add_index_field 'artists_sms', :label => 'Artists'
           config.add_index_field 'art_piece_building_location_ss', :label => 'Location'
