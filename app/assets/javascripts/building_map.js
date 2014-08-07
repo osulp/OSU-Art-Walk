69e2f47e2d681d293ddc93cff59708efc2c66af1
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-08-07 13:44:39 -0700

    added debugging line and deleted files

--- app/assets/javascripts/building_map.js
+++ app/assets/javascripts/building_map.js
@@ -1,45 +1,43 @@
 $(document).ready(function(){
+  $('.myMap').each(function(){
+    var map = L.map('map', {
+      center: [44.563781, -123.279444],
+      zoom: 16
+    });
 
-  //initializes map
-  var map = L.map('map', {
-    center: [44.563781, -123.279444],
-    zoom: 16
-  });
+    //set custom path to marker icon
+    var markerIcon = new L.Icon({
+      iconUrl: 'http://www.lscarolinas.net/assets/leaflet/images/marker-icon-blue.png',
+      iconAnchor: [13, 12]
+    });
 
-  //set custom path to marker icon
-  var markerIcon = new L.Icon({
-    iconUrl: 'http://www.lscarolinas.net/assets/leaflet/images/marker-icon-blue.png',
-    iconAnchor: [13, 12]
-  })
+    //add map to window
+    window.map = map;
 
-  //add map to window
-  window.map = map;
-
-  //set up which map service is used and set zoom params
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18
-  }).addTo(map);
-
-  //add marker to map
-
-  if($('#building_lat').val() != 44.563781){
-    var marker = new L.marker([$('#building_lat').val(), $('#building_long').val()], {
-      draggable: true,
-      icon: markerIcon
+    //set up which map service is used and set zoom params
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      maxZoom: 18
     }).addTo(map);
-  } else {
+
+    //add marker to map
+
     var marker = new L.marker([44.563781, -123.279444], {
-      draggable: true,
-      icon: markerIcon
-    }).addTo(map);
-  }
+        draggable: true,
+        icon: markerIcon
+      }).addTo(map);
 
-  console.log($('#building_lat').val())
+    if($('#building_lat').val() != 0){
+      marker.setLatLng([$('#building_lat').val(), $('#building_long').val()]);
+    }
 
-  map.on('mouseup', function(e){marker.setLatLng(e.latlng)});
-  map.on('mousedown', injectCoords);
+    console.log($('#building_lat').val())
+
+    map.on('mouseup', function(e){marker.setLatLng(e.latlng)});
+    map.on('mousedown', injectCoords);
+  });
 });
+
 
 function injectCoords(e, marker){
   $('#building_lat').val(e.latlng.lat);
