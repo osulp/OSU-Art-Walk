0bc96913a9ef89506a1f94905f1417fa455b59c5
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-08-08 13:53:05 -0700

    altered tests for less duplication

--- spec/features/artist_bio_show_spec.rb
+++ spec/features/artist_bio_show_spec.rb
@@ -2,33 +2,26 @@ require 'spec_helper'
 
 describe "showing artists with bios" do
   let(:art_piece) {create(:art_piece, :with_artist)}
-  context "when viewing an art pieces' show page and the art piece has an artist" do
+  context "when viewing an art pieces' show page" do
     before do
       visit catalog_path(:id => art_piece.document_id)
     end
-
-    it "should display that art piece's artist" do
-      expect(page).to have_content("Artist")
+    context "and the art piece has an artist" do
+      it "should display that art piece's artist" do
+        expect(page).to have_content("Artist")
+      end
     end
-  end
-  context "when viewing an art pieces' show page and the art piece does not have an artist" do
-    let(:art_piece) {create(:art_piece)}
-    before do
-      visit catalog_path(:id => art_piece.document_id)
+    context "and the art piece does not have an artist" do
+      let(:art_piece) {create(:art_piece)}
+      it "should not display that art_piece's artist" do
+        expect(page).to_not have_content("Artist")
+      end
     end
-
-    it "should not display that art_piece's artist" do
-      expect(page).to_not have_content("Artist")
-    end
-  end
-  context "when viewing an art pieces' show page and the art piece has many artists" do
-    let(:art_piece) {create(:art_piece, :with_artists)}
-    before do
-      visit catalog_path(:id => art_piece.document_id)
-    end
-
-    it "should display that art piece's artists" do
-      expect(page).to have_content("Artist:", :count => 2)
+    context "and the art piece has many artists" do
+      let(:art_piece) {create(:art_piece, :with_artists)}
+      it "should display that art piece's artists" do
+        expect(page).to have_content("Artist:", :count => 2)
+      end
     end
   end
 end
