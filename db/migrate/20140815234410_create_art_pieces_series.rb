bd7297820c900327af0ea7deb7bf9cabc261669c
balesh2 <balesh@onid.oregonstate.edu>
2014-08-18 14:34:38 -0700

    removes duplicate migration with old filename

--- db/migrate/20140815234410_create_art_pieces_series.rb
+++
@@ -1,8 +0,0 @@
-class CreateArtPieceSeries < ActiveRecord::Migration
-  def change
-    create_table :art_piece_series do |t|
-      t.references :art_pieces, :index => true
-      t.references :series, :index => true
-    end
-  end
-end
