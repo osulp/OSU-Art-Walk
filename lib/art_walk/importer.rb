7790b56ee87e7c1d02d53d9f684bb155dedc3162
Brandon Straley <straleyb@onid.oregonstate.edu>
2014-08-19 11:32:36 -0700

    added building importer and specs

---
+++ lib/art_walk/importer.rb
@@ -0,0 +1,162 @@
+require 'csv'
+
+module ArtWalk
+  class Importer
+    def self.call(file)
+      new(file).import!
+    end
+
+    attr_accessor :file
+
+    def initialize(file)
+      @file = file
+    end
+
+    def import!
+      stub_solr
+      ArtPiece.transaction do
+        rows.each do |row|
+          import_row!(row)
+        end
+      end
+      unstub_solr
+      ArtPiece.reindex
+    end
+
+    def rows
+      @rows ||= CSV.open(file, :headers => true)
+    end
+
+    private
+
+    def stub_solr
+      @old_solr = Sunspot.session
+      Sunspot.session = Sunspot::Rails::StubSessionProxy.new(Sunspot.session)      
+    end
+
+    def unstub_solr
+      Sunspot.session = @old_solr
+    end
+
+    def import_row!(row)
+      RowCreator.call(row)
+    end
+  end
+
+  class RowCreator
+    def self.call(row)
+      new(row).import!
+    end
+
+    attr_accessor :row
+
+    def initialize(row)
+      @row = row
+    end
+
+    def import!
+      assign_art_piece_attributes
+      art_piece.tap(&:save!)
+    end
+
+    private
+
+    def art_piece
+      @art_piece ||= ArtPiece.new
+    end
+
+    def assign_art_piece_attributes
+      row.each do |header, value|
+        header = header.gsub(' ','_').gsub(/[^A-z_]/,'').underscore
+        translate_field(header, value) if translates_field?(header)
+        send(:"assign_#{header}", value) if respond_to? :"assign_#{header}", true
+      end
+    end
+
+    def assign_artist_name(value)
+      new_val = value.split(/[&;]/)
+      new_val.each do |val|
+        new_artist = Artist.find_or_initialize_by(:name => val)
+        new_artist.save
+        art_piece.artists = [new_artist]
+      end
+      art_piece.save
+    end
+
+    def assign_building(value)
+      new_building = Building.find_or_initialize_by(:name => value)
+      new_building.save
+      art_piece.building = new_building
+      art_piece.save
+    end
+
+    def assign_medium(value)
+      new_val = value.to_s.gsub("and", ",").split(/[;,]/).delete_if(&:blank?)
+      art_piece.media = []
+      new_val.each do |val|
+        new_media = Medium.find_or_initialize_by(:medium => val.strip)
+        art_piece.media << new_media
+      end
+      art_piece.save
+    end
+
+    def assign_accessibility(value)
+      if value == "Public"
+        art_piece.private = false
+      else
+        art_piece.private = true
+      end
+      art_piece.contact_info = value
+      art_piece.save
+    end
+
+    def assign_lost_moving_to_be_hidden(value)
+      if value == "FALSE"
+        art_piece.displayed = true
+      else
+        art_piece.displayed = false
+      end
+      art_piece.save
+    end
+
+    def assign_location(value)
+      art_piece.art_piece_building.location = value
+      art_piece.save
+    end
+
+    def assign_number_for_website(value)
+      art_piece.art_piece_building.position_num = value
+      art_piece.save
+    end
+
+    def translates_field?(header)
+      field_mapping.include?(header)
+    end
+
+    def translate_field(header, value)
+      translated_field = field_mapping[header]
+      art_piece.send(:"#{translated_field}=", value)
+    end
+
+    def assign_work_title(value)
+      if value.blank?
+        art_piece.title = "untitled"
+      else
+        art_piece.title = value
+      end
+      art_piece.save
+    end
+
+    def field_mapping
+      {
+        "additional_information_including_all_data_collected_from_plaques_that_accompany_the_pieces" => "description",
+        "year" => "creation_date",
+        "dimensions" => "size",
+        "artist_notes" => "artist_comments",
+        "percent_for_art" => "percent_for_art",
+        "lost_moving_to_be_hidden" => "displayed",
+        "number_on_plaque_valley_nw_art_exhibit_only" => "number"
+      }
+    end
+  end
+end
---
+++ spec/lib/importer_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+csv = Rails.root.join("spec", "fixtures", "fixture_data.csv")
+describe ArtWalk::Importer do
+  let(:file) {csv}
+  subject {ArtWalk::Importer.new(file)}
+
+  describe "#import!" do
+    before do
+      subject.import!
+    end
+    it "should create an art piece" do
+      expect(ArtPiece.all.count).to eq 2
+    end
+  end
+end
+
+describe ArtWalk::RowCreator do
+  let(:file) {csv}
+  let(:row) {ArtWalk::Importer.new(file).rows.first}
+  subject {ArtWalk::RowCreator.new(row)}
+
+  describe "#import!" do
+    let(:result) {subject.import!}
+    it "should return an art piece" do
+      expect(result).to be_kind_of ArtPiece
+    end
+    it "should have a title" do
+      expect(result.title).to eq "Test Title"
+    end
+    it "should have contact info" do
+      expect(result.contact_info).to eq "Public"
+    end
+    it "should have an artist" do
+      expect(result.artists.first.name).to eq "Bob the Builder"
+    end
+    it "should have a building" do
+      expect(result.building.name).to eq "Batcheller Hall"
+    end
+    it "should have a medium" do
+      expect(result.media.first.medium).to eq "mural"
+    end
+    #it "should have many media" do
+    #  expect(result.media.count).to eq 4
+    #end
+    it "should have a description" do
+      expect(result.description).to eq "Test Description"
+    end
+    it "should have a year" do
+      expect(result.creation_date).to eq "1999"
+    end
+    it "should have a size" do
+      expect(result.size).to eq "19 1/4\" x 23 1/4\""
+    end
+    it "should have artist comments" do
+      expect(result.artist_comments).to eq "These are notes from the artist probably"
+    end
+    it "should have percent for art status" do
+      expect(result.percent_for_art).to eq true
+    end
+    it "should have displayed status" do
+      expect(result.displayed).to eq false
+    end
+    it "should have private status" do
+      expect(result.private?).to eq false
+    end
+    it "should have a number" do
+      expect(result.number).to eq "1"
+    end
+    it "should have an art piece building position number" do
+      expect(result.art_piece_building.position_num).to eq 1
+    end
+    it "should have an art piece building location" do
+      expect(result.art_piece_building.location).to eq "second floor, near room 241, through KOAC door"
+    end
+    context "if the title that is read in is blank" do
+      let(:row) {ArtWalk::Importer.new(file).rows.to_a.last}
+
+      it "should have untitled as the title" do
+        expect(result.title).to eq "untitled"
+      end
+    end
+  end
+end
