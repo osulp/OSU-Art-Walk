15a00b6fcaaa86c02bfaa9039cfa666f3e9bcfc0
balesh2 <balesh@onid.oregonstate.edu>
2014-09-19 08:53:41 -0700

    gets rid of code duplication

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -42,21 +42,14 @@ module MapHelper
       if url_opts === false || url_opts[:suppress_link]
         value
       else
-        link_to_photo_document doc, url_opts.merge(:label => value)
+        link_to_map_document doc, url_opts.merge(:label => value)
       end
     end
-  end
-  def link_to_photo_document(doc, opts={:label => nil, :counter => nil})
-    opts[:label] ||= document_show_link_field(doc)
-    label = render_document_index_label doc, opts
-    search_context = opts[:search_context].to_json if opts[:search_context].present?
-    link_to label, catalog_path(doc, :search_context => search_context), document_link_params(doc, opts)
   end
 
   def map_building(document)
     return nil unless params[:action] == "index" && cleaned_params.blank?
     {:f => {:building_ss => document["building_ss"]}, :sort => "art_piece_building_position_num_is asc"}
   end
-
 
 end
